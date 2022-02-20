586f65e9804dbea2d694a2e565dd24d30a7c23b6
Yifei Zhang <yidadaa@qq.com>
2022-02-20 16:26:56 +0800

    Update custom.js

--- .github/workflows/custom.js
+++ .github/workflows/custom.js
@@ -41,7 +41,7 @@ let mainConfig = {
       },
       {
         name: "Resume",
-        link: "https://cdn.jsdelivr.net/gh/Yidadaa/Coding-Every-Day@master/2022/cv.pdf"
+        link: "/cv"
       }
     ],
     headTitle: ["暮春早夏的月亮", "原是情人的月亮，不比秋冬是诗人的月亮"],
