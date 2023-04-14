e2a8dfabfc5f316103c3964110825e4796c15d24
Yidadaa <yidadaa@qq.com>
2023-04-14 02:56:07 +0000

    deploy(blog): version 4/14/2023, 2:56:05 AM

---
+++ _nuxt/static/1681440964/posts/31/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681440964",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002F31",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
